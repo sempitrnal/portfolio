b71cd703465c345dc3818d5b97c07ada3e4959c0
Reynald Sampelo <reynalsampelo@gmail.com>
2022-12-01 14:48:51 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,13 @@ import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 import Head from "next/head";
 import SocialLinks from "../components/SocialLinks";
+import { BsArrowBarRight, BsDownload } from "react-icons/bs";
+import { FiDownload } from "react-icons/fi";
+import { tech_stack } from "../utils/tech_stack";
+import StackCard from "../components/StackCard";
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
 
 /* eslint-disable react/no-unescaped-entities */
 export const slideDown = {
@@ -30,14 +37,21 @@ export const slideDown = {
 	},
 };
 export default function Home() {
-	const [setTheme, colorTheme] = useDarkMode();
-
+	const [isLoading, setLoading] = useState(true);
 	return (
-		<motion.div className="min-h-screen transition duration-500 ease-in-out dark:text-white">
+		<motion.div className="min-h-screen ease-in-out dark:text-white page">
 			<Head>
 				<title>Reynald Sampelo</title>
+				<meta
+					name="description"
+					content="	An Information Technology student who is passionate and has innate
+					curiosity about Software Development. Aspiring
+					to be a full-stack developer who can adapt quickly and easily grasp
+					emerging technologies."
+				/>
 			</Head>
-			<div className="flex flex-col items-center justify-center h-screen">
+
+			{/* <div className="flex flex-col items-center justify-center h-screen">
 				<div className="flex text-7xl md:text-9xl">
 					<h1 className="mb-5 md:mb-10 ">Building</h1>
 					<Typewriter
@@ -53,8 +67,8 @@ export default function Home() {
 				<h3 className="md:text-xl">
 					Still working on my website <span className="mx-2">😄</span>
 				</h3>
-			</div>
-			{/* <motion.div
+			</div> */}
+			<motion.div
 				initial="initial"
 				animate="animate"
 				exit="exit"
@@ -62,40 +76,113 @@ export default function Home() {
 				variants={slideDown}
 				className=""
 			>
-				<motion.h2 className="mb-5 text-6xl font-semibold text-black dark:text-white sub">
-					Hello, I'm{" "}
-					<span className="text-transparent  bg-gradient-to-r from-[#191654] to-[#43c6ac] dark:from-[#7069f4] dark:to-[#abedf3] bg-clip-text">
-						Reynald
-					</span>{" "}
-					👋🏽
-				</motion.h2>
+				<div className="flex flex-col items-center justify-between gap-10 lg:items-end lg:flex-row">
+					<div className="relative lg:hidden w-[20rem] h-[20rem]">
+						<Image src={"/bo.png"} layout="fill" objectFit="contain" alt="" />
+					</div>
+					<div className="flex flex-col items-center text-center lg:items-start lg:text-start">
+						<motion.h2 className="mb-5 text-6xl font-semibold text-black transition duration-500 dark:text-white">
+							Hello, I'm{" "}
+							<span className="text-transparent transition-all duration-1000 delay-1000 bg-gradient-to-r from-[#191654] to-[#43c6ac] dark:from-[#7069f4] dark:to-[#abedf3] bg-clip-text">
+								Reynald
+							</span>{" "}
+							👋🏽
+						</motion.h2>
 
-				<motion.h2 className="text-2xl text-[#000000a7] dark:text-[#b0b0b0]  mb-5">
-					Cebu Institute of Technology - University
-				</motion.h2>
-				<div className="mb-5">
-					<SocialLinks />
+						<motion.h2 className="text-2xl duration-500  text-[#000000a7] dark:text-[#b0b0b0]  mb-5">
+							Cebu Institute of Technology - University
+						</motion.h2>
+						<div className="mb-7">
+							<SocialLinks />
+						</div>
+						<a
+							className="flex mt-5  items-center justify-start bg-[#3c3c3c] w-max text-white rounded-md px-4 hover:opacity-90 transition-opacity duration-500 mb-5 py-2	 gap-3"
+							href="/Reynald_Sampelo_Resume.pdf"
+							download
+						>
+							Resume{" "}
+							<div className="text-xl">
+								<FiDownload />
+							</div>
+						</a>
+					</div>
+					<div className="hidden w-[15rem] h-[15rem] overflow-hidden rounded-full  lg:block relative lg:w-[22rem] lg:h-[22rem]">
+						<Image
+							src={"/bo.png"}
+							layout="fill"
+							objectFit="contain"
+							alt=""
+							className={`ease-in-out duration-[.6s] ${
+								isLoading ? "opacity-0 blur-md" : "opacity-100 blur-0"
+							}`}
+							onLoadingComplete={() => setLoading(false)}
+						/>
+					</div>
 				</div>
-				<motion.p className="w-full md:w-[60%] text-[#0008] dark:text-[#595959] mb-5">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro harum
-					fugiat est ratione suscipit quo pariatur eveniet rerum voluptatem non!
-					Tempore, quis explicabo maxime cupiditate quibusdam nobis fugiat
-					inventore eum.
-				</motion.p>
-
-				<div className="flex gap-1 text-xl font-semibold">
-					<p>I am </p>
+				{/* <div className="flex gap-1 text-xl font-semibold">
 					<Typewriter
 						options={{
-							strings: ["JavaScript", "React", "Next"],
+							strings: ["PHP", "JavaScript", "React", "NextJS"],
 							autoStart: true,
 							loop: true,
 							pauseFor: 1500,
 							deleteSpeed: 100,
 						}}
 					/>
+				</div> */}
+				<h2 className="header2">About</h2>
+
+				<motion.p className="paragraph">
+					I am an Information Technology student who is passionate and has
+					innate curiosity about{" "}
+					<span className="font-semibold"> Software Development. </span>I aspire
+					to be a full-stack developer who can adapt quickly and easily grasp
+					emerging technologies.
+				</motion.p>
+				<p className="paragraph">
+					I am interested in learning more about Web and Mobile Application
+					Development. I am comfortable on working with{" "}
+					<span className="font-semibold">ReactJS</span> and{" "}
+					<span className="font-semibold">NextJS</span> on my projects. I am
+					also yet to learn about <span className="font-semibold">BaaS</span>{" "}
+					such as <span className="font-semibold">Firebase </span> &{" "}
+					<span className="font-semibold">Supabase</span> to integrate on my
+					projects. I also ought to start learning{" "}
+					<span className="font-semibold">React Native </span> as my main Mobile
+					App Tech Stack.
+				</p>
+				<p className="paragraph">
+					Other than coding, I enjoy doing{" "}
+					<span className="font-semibold">Yaw-Yan</span> (a Filipino martial
+					art), <span className="font-semibold">Surfskating</span>,{" "}
+					<span className="font-semibold">Freediving</span>,{" "}
+					<span className="font-semibold">Photography</span>,{" "}
+					<span className="font-semibold">Videography</span>, and playing{" "}
+					<span className="font-semibold">musical instruments</span> in my free
+					time.
+				</p>
+
+				<h2 className="header2">Stack</h2>
+				<div className="flex flex-wrap w-full gap-6 my-10 mb-24">
+					{tech_stack.map((e) => {
+						return <StackCard key={e.name} e={e} />;
+					})}
 				</div>
-			</motion.div> */}
+				<div className="flex flex-col items-center justify-center  mt-[12rem] mb-[5rem] ">
+					<p className="text-[4.5rem] text-[#292929] dark:text-white">
+						next up
+					</p>
+					<Link href={"/experience"} scroll={false}>
+						<motion.p
+							whileHover={{ x: 10 }}
+							transition={{ duration: 0.2 }}
+							className="flex gap-2 items-center text-[3rem] font-thin cursor-pointer"
+						>
+							Experience -{">"}
+						</motion.p>
+					</Link>
+				</div>
+			</motion.div>
 		</motion.div>
 	);
 }
